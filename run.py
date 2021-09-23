252547920b0d839cc31f4281d4320a116fafd0d8
Marcel Santos <mss8@cin.ufpe.br>
2021-09-23 11:31:30 -0300

    Fix typo.

--- run.py
+++ run.py
@@ -184,6 +184,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    input_path = args.input_path
     output_path = args.output_path if args.output_path else input_path
 
     # set extension to save depth files.
